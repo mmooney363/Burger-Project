b64b05b997297244ed0b8c5216cac9b5fb39cd5a
Mike Mooney <mmooney363@gmail.com>
2019-03-07 23:11:33 -0500

    added files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Homestyle Mushroom Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwestern Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chile Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Rum Onion Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Burger', true);
